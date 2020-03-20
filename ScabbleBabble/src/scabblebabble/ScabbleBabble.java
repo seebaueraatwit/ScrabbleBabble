355226c23f746a9b0ef418888baf04ee467e0977
Aubri Seebauer <seebauera@wit.edu>
2020-03-20 15:06:34 -0400

    Commit 0.0.1 HelloWold Test A

--- ScabbleBabble/src/scabblebabble/ScabbleBabble.java
+++ ScabbleBabble/src/scabblebabble/ScabbleBabble.java
@@ -1,6 +1,9 @@
 package scabblebabble;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class ScabbleBabble  extends Application {
@@ -14,6 +17,14 @@ public class ScabbleBabble  extends Application {
 		s.setWidth(800);
 		s.setHeight(600);
 		
+		s.setTitle("Hello World!");
+		final Button btn = new Button();
+		btn.setText("Click Me!");
+		
+		final StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		
+		s.setScene(new Scene(root, 300, 250));
 		s.show();
 	}
 
