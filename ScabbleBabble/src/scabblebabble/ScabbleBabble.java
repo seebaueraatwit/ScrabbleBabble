ca216e906e7d87dd2cec1dfb375ee0219bc82516
Aubri Seebauer <seebauera@wit.edu>
2020-03-20 14:57:23 -0400

    Initial Commit

---
+++ ScabbleBabble/src/scabblebabble/ScabbleBabble.java
@@ -0,0 +1,21 @@
+package scabblebabble;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ScabbleBabble  extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage s) throws Exception {
+		s.setWidth(800);
+		s.setHeight(600);
+		
+		s.show();
+	}
+
+	
+}
