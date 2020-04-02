15e09785d77087d68adc2672a4bb17d960e30860
Aubri Seebauer <seebauera@wit.edu>
2020-04-02 18:28:40 -0400

    Ready for FXML rewrite

--- ScabbleBabble/src/scrabblebabble/ScrabbleBabble.java
+++ ScabbleBabble/src/scrabblebabble/ScrabbleBabble.java
@@ -1,15 +1,19 @@
 package scrabblebabble;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import scrabblebabble.board.Board;
 
-public class ScrabbleBabble  extends Application {
+public class ScrabbleBabble  extends Application implements Initializable {
 
 	public static Board board;
+	
+	public Button button1;
 	
 	public static void main(String[] args) {
 		board = new Board();
@@ -19,7 +23,13 @@ public class ScrabbleBabble  extends Application {
 	@Override
 	public void start(Stage s) throws Exception {
 		
+		//TODO the fxml is made but needs to be filled
 		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
 		
 	}
 
